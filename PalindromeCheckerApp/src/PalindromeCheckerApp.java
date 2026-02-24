16676d1a30e469f4ad781d3cff3b8a25646db486
hemagiri-rs <hs2070@srmist.edu.in>
2026-02-24 15:37:50 +0530

    palindrome checker App

--- PalindromeCheckerApp/src/PalindromeCheckerApp.java
+++ PalindromeCheckerApp/src/PalindromeCheckerApp.java
@@ -1,12 +1,16 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args){
         String str = "madam";
-        str = str.toLowerCase();
-        String reversed = new StringBuilder(str).reverse().toString();
-        if (str.equals(reversed)) {
-            System.out.println(str + " is a palindrome.");
+        String reversed = "";
+        for(int i = str.length() -1;i >=0; i--){
+            reversed = reversed + str.charAt(i);
+
+        }
+        if(str.equals(reversed)) {
+            System.out.println(str + " is a palindrome");
+
         } else {
-            System.out.println(str + " is not a palindrome.");
+            System.out.println(str+ " is not a palindrome");
         }
 
     }
